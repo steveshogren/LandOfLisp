79573ac894fcc46d0eb4f0c1cd99c624eed26f87
Steve <madklowns@gmail.com>
2011-06-11 08:24:22 -0400

    Adding undirected graph support

--- guess-my-number.lisp
+++ guess-my-number.lisp
@@ -211,3 +211,28 @@ is a giant welding torch in the corner.))))
 			(lambda ()
 			  (graph->dot nodes edges))))
 
+(defun uedges->dot (edges)
+  (maplist (lambda (lst)
+			 (mapc (lambda (edge)
+					 (unless (assoc (car edge) (cdr lst))
+					   (fresh-line)
+					   (princ (dot-name (caar lst)))
+					   (princ "--")
+					   (princ (dot-name (car edge)))
+					   (princ "[label=\"")
+					   (princ (dot-label (cdr edge)))
+					   (princ "\"];")))
+				   (cdar lst)))
+		   edges))
+
+(defun ugraph->dot (nodes edges)
+  (princ "graph{")
+  (nodes->dot nodes)
+  (uedges->dot edges)
+  (princ "}"))
+
+(defun ugraph->png (fname nodes edges)
+  (dot->png fname
+			(lambda ()
+			  (ugraph->dot nodes edges))))
+
