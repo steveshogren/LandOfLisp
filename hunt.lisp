4af1c533a0cca20f7f712c7592bdac1f9c4d1bfb
Steve <madklowns@gmail.com>
2011-06-17 07:06:12 -0400

    Adding a fuction to detect the glow worms and wumpus by calculating the node distances. Also, made a function to make a data list of all the node attibutes in the whole graph.

--- hunt.lisp
+++ hunt.lisp
@@ -54,4 +54,64 @@
 (defun connect-all-islands (nodes edge-list)
   (append (connect-with-bridges (find-islands nodes edge-list)) edge-list))
 
+(defun make-city-edges ()
+  (let* ((nodes (loop for i from 1 to *node-num*
+					  collect i))
+		 (edge-list (connect-all-islands nodes (make-edge-list)))
+		 (cops (remove-if-not (lambda (x)
+								(zerop (random *cop-odds*)))
+							  edge-list)))
+	(add-cops (edges-to-alist edge-list) cops)))
 
+(defun edges-to-alist (edge-list)
+  (mapcar (lambda (node1)
+			(cons node1
+				  (mapcar (lambda (edge)
+							(list (cdr edge)))
+						  (remove-duplicates (direct-edges node1 edge-list)
+											 :test #'equal))))
+		  (remove-duplicates (mapcar #'car edge-list))))
+
+(defun add-cops (edge-alist edges-with-cops)
+  (mapcar (lambda (x)
+			(let ((node1 (car x))
+				  (node1-edges (cdr x)))
+			  (cons node1
+					(mapcar (lambda (edge)
+							  (let ((node2 (car edge)))
+								(if (intersection (edge-pair node1 node2)
+												  edges-with-cops
+												  :test #'equal)
+								  (list node2 'cops)
+								  edge)))
+							node1-edges))))
+		  edge-alist))
+
+(defun neighbors (node edge-alist)
+  (mapcar #'car (cdr (assoc node edge-alist))))
+
+(defun within-one (a b edge-alist)
+  (member b (neighbors a edge-alist)))
+
+(defun within-two (a b edge-alist)
+  (or (within-one a b edge-alist)
+	  (some (lambda (x)
+			  (within-one x b edge-alist))
+			(neighbors a edge-alist ))))
+
+(defun make-city-nodes (edge-alist)
+  (let ((wumpus (random-node))
+		(glow-worms (loop for i below *worm-num*
+						  collect (random-node))))
+	(loop for n from 1 to *node-num*
+		  collect (append (list n)
+						  (cond ((eql n wumpus) '(wumpus))
+								((within-two n wumpus edge-alist) '(blood!)))
+						  (cond ((member n glow-worms)
+								 '(glow-worm))
+								((some (lambda (worm)
+										 (within-one n worm edge-alist))
+									   glow-worms)
+								 '(lights!)))
+						  (when (some #'cdr (cdr (assoc n edge-alist)))
+							'(sirens!)))))))
